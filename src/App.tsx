50eb4aae94eb2e13835ac357f1598d64a548e474
graceyiyo-cloud <graceyiyo@gmail.com>
2026-07-04 21:14:36 +0800

    fix: allow importing non-JSON backup files
    Remove the .json file extension restriction to support various backup file formats.

--- src/App.tsx
+++ src/App.tsx
@@ -2899,7 +2899,7 @@ ${categoryOptions}
                       <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" x2="12" y1="3" y2="15"/></svg>
                       從備份檔還原
                     </button>
-                    <input id="backupFileInput" type="file" accept=".json" className="hidden" onChange={handleImportBackup} />
+                    <input id="backupFileInput" type="file" className="hidden" onChange={handleImportBackup} />
                   </div>
                 </div>
               </div>
